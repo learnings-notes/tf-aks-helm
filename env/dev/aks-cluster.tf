51c4a4b06746af08d7cf2b5149153f539aa4db7e
jamalikake <jamalirkp@gmail.com>
2022-12-27 00:16:47 +0530

    Update aks-cluster.tf

--- env/dev/aks-cluster.tf
+++ env/dev/aks-cluster.tf
@@ -1,19 +1,5 @@
 
 
-provider "azurerm" {
-  features {}
-}
-provider "azurerm" {
-  features {}
-}
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstatefile-rg"
-    storage_account_name = "ghtfstatefilesa"
-    container_name       = "rgtfstatecontainer"
-    key                  = "terraformtemplateDev.tfstate"
-  }
-}
 
 resource "azurerm_resource_group" "default" {
   name     = "test-k8s-delete-A"
